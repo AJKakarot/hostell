92be637430bd317bbbaeeccc56c114388e02ac47
ajeet gupta <140450935+AJKakarot@users.noreply.github.com>
2025-09-14 17:58:43 +0530

    final

--- hostelManagement/server/index.js
+++ hostelManagement/server/index.js
@@ -1,10 +1,14 @@
+require('dotenv').config(); // <-- must be first
 const app = require('./config/express');
-const config = require('./config/config')
+const config = require('./config/config');
+const env = process.env.NODE_ENV || 'development';
 
 // initialize mongo
 require('./config/mongoose');
+const passport = require('./middleware/passport');
 
-// listen to the port 
+
+// listen to the port
 app.listen(config.port, () => {
     console.log(`listening on port ${config.port} (${config.env})`);
 });
--- hostelManagement/server/middleware/passport.js
+++ hostelManagement/server/middleware/passport.js
@@ -1,40 +1,37 @@
+console.log('Loading passport middlewar',process.env.NODE_ENV);
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
 
-const config = require('../config/config');
 const userController = require('../controllers/user.controller');
 
+console.log('JWT Secret:', process.env.JWT_SECRET); // debug
+
 const localLogin = new LocalStrategy(
-    {
-        usernameField: 'username',
-        passwordField: 'password',
-    },
-    async(username, password, done) => {
-        const user = userController.getUserByUsernameAndPassword(username, password);
-        return user
-        ?done(null, user) 
-        :done(null, false, {
-            error: 'Your login Details are not valid. Please try again'
-        });
-    }
+  {
+    usernameField: 'username',
+    passwordField: 'password',
+  },
+  async (username, password, done) => {
+    const user = await userController.getUserByUsernameAndPassword(username, password);
+    return user
+      ? done(null, user)
+      : done(null, false, { error: 'Your login details are not valid.' });
+  }
 );
 
-
 const jwtLogin = new JwtStrategy(
-    {
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey: config.jwtSecret
-    },
-    async(payload, done) => {
-        const user = await userController.getUserById(payload._id);
-        return user
-        ?done(null, user) 
-        :done(null, false, {
-            error: 'Your login Details are not valid. Please try again'
-        });
-    }
+  {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+  },
+  async (payload, done) => {
+    const user = await userController.getUserById(payload._id);
+    return user
+      ? done(null, user)
+      : done(null, false, { error: 'Your login details are not valid.' });
+  }
 );
 
 module.exports = passport.use(localLogin).use(jwtLogin);
